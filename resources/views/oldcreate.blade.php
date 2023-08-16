20df56a5c225d71caeb5f953f50be0407b4a861b
Marco Giammari <marcogiammari@gmail.com>
2023-08-16 12:16:59 +0200

    reusing blade code - subviews

---
+++ resources/views/oldcreate.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('title', 'Add New Task')
+
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0.8rem;
+        }
+    </style>
+@endsection
+
+@section('content')
+
+    {{-- laravel restituisce un oggetto $errors che contiene tutti gli errori generati dalla validazione --}}
+    {{-- {{ $errors }} --}}
+    {{-- per accedere al messaggio invece si usa la variabile $message  --}}
+
+    <form action="{{ route('tasks.store') }}" method="post">
+
+        {{-- il csrf attacca un cookie al browser per verifiche di sicurezza. Può essere un file, ma per progetti più importanti si usa Redis (un db), memcached o altri metodi che trovi in config.session --}}
+        @csrf
+        <div>
+            <label for="title">
+                Title
+            </label>
+
+            {{-- la funzione old ci permette di recuperare un valore corretto inserito in una form request non validata. Non lo usare per dati sensibili come le password. --}}
+
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="description">
+                Description
+            </label>
+            <textarea name="description" id="description" rows="5">{{ old('description') }}
+            </textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="long_description">
+                Description
+            </label>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}
+        </textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
---
+++ resources/views/oldedit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Task')
+
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0.8rem;
+        }
+    </style>
+@endsection
+
+@section('content')
+
+    {{-- laravel restituisce un oggetto $errors che contiene tutti gli errori generati dalla validazione --}}
+    {{-- {{ $errors }} --}}
+    {{-- per accedere al messaggio invece si usa la variabile $message  --}}
+
+    <form action="{{ route('tasks.update', ['task' => $task->id]) }}" method="post">
+
+        {{-- il csrf attacca un cookie al browser per verifiche di sicurezza. Può essere un file, ma per progetti più importanti si usa Redis (un db), memcached o altri metodi che trovi in config.session --}}
+        @csrf
+
+        {{-- aggiunge ai form data una proprietà _method: 'PUT'  --}}
+        @method('PUT')
+        <div>
+            <label for="title">
+                Title
+            </label>
+
+            {{-- qui l'old va usato con il null coalescing operator --}}
+
+            <input type="text" name="title" id="title" value="{{ old('title') ?? $task->title }}">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="description">
+                Description
+            </label>
+            <textarea name="description" id="description" rows="5">{{ old('description') ?? $task->description }}
+            </textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="long_description">
+                Description
+            </label>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') ?? $task->long_description }}
+        </textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <button type="submit">Edit Task</button>
+        </div>
+    </form>
+@endsection
