66ad4f5ac3ba68376cb330c5e546c3eaae46053d
Marco Giammari <marcogiammari@gmail.com>
2023-08-16 19:48:31 +0200

    styling forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,14 +4,14 @@
 
 @section('title', isset($task) ? 'Edit Task' : 'Add New Task')
 
-@section('styles')
+{{-- @section('styles')
     <style>
         .error-message {
             color: red;
             font-size: 0.8rem;
         }
     </style>
-@endsection
+@endsection --}}
 
 @section('content')
 
@@ -21,43 +21,47 @@
         @isset($task)
             @method('PUT')
         @endisset
-        <div>
+        <div class="mb-4">
             <label for="title">
                 Title
             </label>
 
-            <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}">
+            {{-- la direttiva blade @class assegna la classe border-etc all'input se c'è un errore nel titolo, grazie al metodo built-in di $errors has('title'). In alternativa è possibile usare la direttiva @error all'interno dell'attributo class dell'input --}}
+            <input type="text" name="title" id="title" @class(['border-red-500' => $errors->has('title')])
+                value="{{ $task->title ?? old('title') }}">
             @error('title')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="description">
                 Description
             </label>
-            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}
+            <textarea @class(['border-red-500' => $errors->has('description')]) name="description" id="description" rows="5">{{ $task->description ?? old('description') }}
             </textarea>
             @error('description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="long_description">
                 Description
             </label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}
+            <textarea @class(['border-red-500' => $errors->has('long_description')]) name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}
         </textarea>
             @error('long_description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
-            <button type="submit">
+        <div class="flex items-center gap-2">
+            <button class="btn" type="submit">
                 @isset($task)
                     Edit Task
                 @else
                     Add Task
-                @endisset </button>
+                @endisset
+            </button>
+            <a href="{{ route('tasks.index') }}" class="link">Cancel</a>
         </div>
     </form>
 @endsection
